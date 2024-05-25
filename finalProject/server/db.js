c7e86ff136682abceb25e8a8fcf56248a89a49db
JakeBasa <basajake28@gmail.com>
2024-05-25 22:14:37 +0800

    edited db

--- finalProject/server/db.js
+++ finalProject/server/db.js
@@ -1,15 +1,11 @@
 const mongoose = require("mongoose");
 
 module.exports = () => {
-  const connectionParams = {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  };
   try {
-    mongoose.connect(process.env.DB, connectionParams);
+    mongoose.connect(process.env.DB);
     console.log("Connected to database successfully");
   } catch (error) {
     console.log(error);
-    console.log("Could not connect database!");
+    console.log("Could not connect to the database!");
   }
 };
